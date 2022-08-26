56b78437329eafa4a510c27887de9644fb55733d
unknown <quentin.dassas@gmail.com>
2022-08-26 17:49:11 +0200

    administratrion-front

--- picom/src/app/model/tarif.ts
+++ picom/src/app/model/tarif.ts
@@ -2,7 +2,4 @@ export class Tarif {
 
   id: number = 0;
   prixEnEuros: number = 0;
-  administrateur: Administrateur = new Administrateur();
-  zone: Zone = new Zone();
-  tranchesHoraire: TranchesHoraire = new TranchesHoraire();
 }
